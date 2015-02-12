2eb49558a776b96a792400263dae5b993cbc65ec
vinchan <vinchan33@gmail.com>
2015-02-11 19:07:11 -0800

    deleted unnecessary files

--- radlibDemoApp/platforms/android/ant-build/classes.dex.d
+++ radlibDemoApp/platforms/android/ant-build/classes.dex.d
@@ -1,4 +1,11 @@
 C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes.dex : \
+C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\com\example\hello\BuildConfig.class \
+C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\com\example\hello\CordovaApp.class \
+C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\com\example\hello\R$attr.class \
+C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\com\example\hello\R$drawable.class \
+C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\com\example\hello\R$string.class \
+C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\com\example\hello\R$xml.class \
+C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\com\example\hello\R.class \
 C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\com\example\lfdemo\BuildConfig.class \
 C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\com\example\lfdemo\CordovaApp.class \
 C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\com\example\lfdemo\R$attr.class \
@@ -6,10 +13,23 @@ C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-buil
 C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\com\example\lfdemo\R$string.class \
 C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\com\example\lfdemo\R$xml.class \
 C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\com\example\lfdemo\R.class \
+C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\com\megster\cordova\BluetoothSerial$1.class \
+C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\com\megster\cordova\BluetoothSerial.class \
+C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\com\megster\cordova\BluetoothSerialService$AcceptThread.class \
+C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\com\megster\cordova\BluetoothSerialService$ConnectedThread.class \
+C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\com\megster\cordova\BluetoothSerialService$ConnectThread.class \
+C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\com\megster\cordova\BluetoothSerialService.class \
 C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\com\radlib\cordova\BluetoothComm$1$1.class \
 C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\com\radlib\cordova\BluetoothComm$1.class \
 C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\com\radlib\cordova\BluetoothComm$BluetoothThread.class \
 C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\com\radlib\cordova\BluetoothComm.class \
+C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\com\radlib\cordova\BluetoothScanner$1$1.class \
+C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\com\radlib\cordova\BluetoothScanner$1$2.class \
+C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\com\radlib\cordova\BluetoothScanner$1.class \
+C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\com\radlib\cordova\BluetoothScanner$BluetoothThread.class \
+C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\com\radlib\cordova\BluetoothScanner.class \
+C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\org\apache\cordova\api\Dummy.class \
+C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\org\apache\cordova\device\Device.class \
 C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\org\apache\cordova\file\ContentFilesystem.class \
 C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\org\apache\cordova\file\DirectoryManager.class \
 C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\org\apache\cordova\file\EncodingException.class \
@@ -52,4 +72,6 @@ C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-buil
 C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\org\apache\cordova\file\LocalFilesystemURL.class \
 C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\org\apache\cordova\file\NoModificationAllowedException.class \
 C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\org\apache\cordova\file\TypeMismatchException.class \
+C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\org\apache\cordova\networkinformation\NetworkManager$1.class \
+C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\classes\org\apache\cordova\networkinformation\NetworkManager.class \
 C:\Users\Vincent\Desktop\radlib-cordova\radlibDemoApp\platforms\android\ant-build\dexedLibs\classes-725aa256edb26c82223b47e7768c068c.jar \
